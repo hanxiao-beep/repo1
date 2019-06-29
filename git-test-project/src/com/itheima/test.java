932cd29ef5f973a5013fc26ecb7247a90ffbfab7
hanxiao <348675993@qq.com>
2019-06-29 20:33:31 +0800

    测试

---
+++ git-test-project/src/com/itheima/test.java
@@ -0,0 +1,16 @@
+package com.itheima;
+
+public class test {
+    public static void main(String[] args) {
+
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+        System.out.println("qqqqq");
+
+    }
+}
---
+++ git/src/com/itheima/test1.java
@@ -0,0 +1,11 @@
+package com.itheima;
+
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+        System.out.println("1111111");
+
+    }
+}
